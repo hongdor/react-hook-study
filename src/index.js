95e27c710f3176e3c48eca4a53cdacc0954ee92a
HDK7 <rlarhkr81@naver.com>
2021-02-10 21:56:00 +0900

    first commt

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Example(){
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+    return console.log(1);
+  });
+
+  return(
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Example/>,
   document.getElementById('root')
 );
 
